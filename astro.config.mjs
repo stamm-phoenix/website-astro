7e205ee3305db05755cf08818dab6959a45c3203
Hugo Berendi <git@hugo-berendi.de>
2025-09-16 14:40:27 +0200

    Add Netlify adapter and update dependencies in package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,15 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  adapter: netlify()
 });
