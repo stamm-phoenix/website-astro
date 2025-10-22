02bcb66b52e046a742c201e8e93d1cc08a198e57
hugo-berendi <git@hugo-berendi.de>
2025-10-22 20:12:52 +0200

    Apply stashed changes to astro.config.mjs and package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import tailwindcss from '@tailwindcss/vite';
-
-import netlify from '@astrojs/netlify';
-
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-
-  vite: {
-    plugins: [tailwindcss()]
-  },
-  adapter: netlify()
+  site: 'https://dev.stamm-phoenix.de',
 });
