0875dad5cd6627b6664abe2f1a58238018966c96
hugo-berendi <git@hugo-berendi.de>
2026-01-25 15:52:05 +0100

    refactor: remove blog collection config

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,16 +1,5 @@
 import { defineCollection, z } from 'astro:content';
 
-const blog = defineCollection({
-  type: 'content',
-  schema: z.object({
-    title: z.string(),
-    date: z.string(),
-    description: z.string().optional(),
-    tags: z.array(z.string()).optional(),
-    author: z.string().optional(),
-  }),
-});
-
 const gruppenstunden = defineCollection({
   type: 'data',
   schema: z.object({
@@ -50,7 +39,6 @@ const homepage = defineCollection({
 });
 
 export const collections = {
-  blog,
   gruppenstunden,
   homepage,
 };
