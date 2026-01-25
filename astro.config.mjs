2bf57540466dbab186b882194f5c59672b7e6484
hugo-berendi <git@hugo-berendi.de>
2026-01-25 16:18:33 +0100

    fix: remove svelte:fragment usage to fix build panic

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  site: 'https://dev.stamm-phoenix.de',
-  adapter: netlify(),
+  output: "static",
+  site: "https://dev.stamm-phoenix.de",
 });
