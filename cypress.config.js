935ac36a6191e95543b7601ff4d7de2b8223b321
hugo-berendi <git@hugo-berendi.de>
2026-01-27 21:11:10 +0100

    Make e2e base URL configurable and tidy styles

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:4321',
+    baseUrl: process.env.CYPRESS_BASE_URL ?? 'http://localhost:4321',
     supportFile: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
