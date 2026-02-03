5330189411705b4f8bc0d6ebd633818d06ec2f38
Nico Welles <nico@welles.email>
2026-02-04 00:15:35 +0100

    WIP /image/{id} request

--- api/lib/environment.ts
+++ api/lib/environment.ts
@@ -4,6 +4,7 @@ export interface Environment {
   AZURE_TENANT_ID: string;
   AZURE_CLIENT_ID: string;
   AZURE_CLIENT_SECRET: string;
+  SHAREPOINT_BASE_URL: string;
   SHAREPOINT_SITE_ID: string;
   SHAREPOINT_LEITUNGSTEAMS_LIST_ID: string;
 }
@@ -13,6 +14,7 @@ export function getEnvironment(context: InvocationContext): Environment | null {
     AZURE_TENANT_ID: process.env.AZURE_TENANT_ID,
     AZURE_CLIENT_ID: process.env.AZURE_CLIENT_ID,
     AZURE_CLIENT_SECRET: process.env.AZURE_CLIENT_SECRET,
+    SHAREPOINT_BASE_URL: process.env.SHAREPOINT_BASE_URL,
     SHAREPOINT_SITE_ID: process.env.SHAREPOINT_SITE_ID,
     SHAREPOINT_LEITUNGSTEAMS_LIST_ID:
       process.env.SHAREPOINT_LEITUNGSTEAMS_LIST_ID,
