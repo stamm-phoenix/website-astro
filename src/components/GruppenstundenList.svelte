7262c3e6de6732b2dd3d84e8f4e9a77050c097d4
hugo-berendi <git@hugo-berendi.de>
2026-02-10 23:15:36 +0100

    Make GroupCards not change height when in the same row with an expanded one

--- src/components/GruppenstundenList.svelte
+++ src/components/GruppenstundenList.svelte
@@ -35,7 +35,7 @@
   }
 </script>
 
-<div class="grid gap-6 md:grid-cols-2">
+<div class="grid gap-6 md:grid-cols-2 items-start">
   {#if gruppenstundenStore.loading}
     <div role="status" aria-live="polite" class="sr-only">
       Gruppenstunden werden geladen...
