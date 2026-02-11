ef3e5cc5c60bb08dbc62a9bea3878c072fdcf1f1
Nico Welles <nico@welles.email>
2026-02-11 11:59:31 +0100

    Add caching mechanism to SharePoint API calls

--- api/lib/blog-list.ts
+++ api/lib/blog-list.ts
@@ -1,5 +1,6 @@
 import {getClient} from "./token";
 import {EnvironmentVariable, getEnvironment} from "./environment";
+import {cachedFetch} from "./cache";
 
 export interface BlogEntry {
     id: string;
@@ -15,54 +16,56 @@ export interface BlogEntry {
 }
 
 export async function getBlogEntries(): Promise<BlogEntry[]> {
-    const client = getClient();
+    return cachedFetch("blog-list", async () => {
+        const client = getClient();
 
-    const SHAREPOINT_HOST_NAME = getEnvironment(
-        EnvironmentVariable.SHAREPOINT_HOST_NAME,
-    );
+        const SHAREPOINT_HOST_NAME = getEnvironment(
+            EnvironmentVariable.SHAREPOINT_HOST_NAME,
+        );
 
-    const SHAREPOINT_SITE_ID = getEnvironment(
-        EnvironmentVariable.SHAREPOINT_SITE_ID,
-    );
+        const SHAREPOINT_SITE_ID = getEnvironment(
+            EnvironmentVariable.SHAREPOINT_SITE_ID,
+        );
 
-    const SHAREPOINT_BLOG_LIST_ID = getEnvironment(
-        EnvironmentVariable.SHAREPOINT_BLOG_LIST_ID,
-    );
+        const SHAREPOINT_BLOG_LIST_ID = getEnvironment(
+            EnvironmentVariable.SHAREPOINT_BLOG_LIST_ID,
+        );
 
-    const response = await client
-        .api(
-            `/sites/${SHAREPOINT_HOST_NAME},${SHAREPOINT_SITE_ID}/lists/${SHAREPOINT_BLOG_LIST_ID}/items`,
-        )
-        .orderby("createdDateTime desc")
-        .expand("fields")
-        .get();
+        const response = await client
+            .api(
+                `/sites/${SHAREPOINT_HOST_NAME},${SHAREPOINT_SITE_ID}/lists/${SHAREPOINT_BLOG_LIST_ID}/items`,
+            )
+            .orderby("createdDateTime desc")
+            .expand("fields")
+            .get();
 
-    const items = Array.isArray(response?.value) ? response.value : [];
+        const items = Array.isArray(response?.value) ? response.value : [];
 
-    const blogEntries: BlogEntry[] = items.map((item: any): BlogEntry => {
-        let imageJson: string | undefined = undefined;
-        if (item.fields.Foto) {
-            try {
-                const parsed = JSON.parse(item.fields.Foto);
-                imageJson = parsed?.fileName || undefined;
-            } catch (e) {
-                imageJson = undefined;
+        const blogEntries: BlogEntry[] = items.map((item: any): BlogEntry => {
+            let imageJson: string | undefined = undefined;
+            if (item.fields.Foto) {
+                try {
+                    const parsed = JSON.parse(item.fields.Foto);
+                    imageJson = parsed?.fileName || undefined;
+                } catch (e) {
+                    imageJson = undefined;
+                }
             }
-        }
-        
-        return {
-            id: item.id,
-            eTag: item.eTag,
-            title: item.fields.Title,
-            content: item.fields.Inhalt,
-            imageFileName: imageJson,
-            hasImage: imageJson != null,
-            createdBy: item.createdBy?.user?.displayName ?? "Unbekannt",
-            createdAt: item.createdDateTime,
-            lastModifiedBy: item.lastModifiedBy?.user?.displayName ?? "Unbekannt",
-            lastModifiedAt: item.lastModifiedDateTime
-        };
-    });
+            
+            return {
+                id: item.id,
+                eTag: item.eTag,
+                title: item.fields.Title,
+                content: item.fields.Inhalt,
+                imageFileName: imageJson,
+                hasImage: imageJson != null,
+                createdBy: item.createdBy?.user?.displayName ?? "Unbekannt",
+                createdAt: item.createdDateTime,
+                lastModifiedBy: item.lastModifiedBy?.user?.displayName ?? "Unbekannt",
+                lastModifiedAt: item.lastModifiedDateTime
+            };
+        });
 
-    return blogEntries;
+        return blogEntries;
+    });
 }
--- api/lib/download-files-list.ts
+++ api/lib/download-files-list.ts
@@ -1,5 +1,6 @@
 import {getClient} from "./token";
 import {EnvironmentVariable, getEnvironment} from "./environment";
+import {cachedFetch} from "./cache";
 
 export interface DownloadFile {
     id: string;
@@ -20,51 +21,53 @@ export interface DownloadFile {
 }
 
 export async function getDownloadFiles(): Promise<DownloadFile[]> {
-    const client = getClient();
+    return cachedFetch("download-files-list", async () => {
+        const client = getClient();
 
-    const SHAREPOINT_HOST_NAME = getEnvironment(
-        EnvironmentVariable.SHAREPOINT_HOST_NAME,
-    );
+        const SHAREPOINT_HOST_NAME = getEnvironment(
+            EnvironmentVariable.SHAREPOINT_HOST_NAME,
+        );
 
-    const SHAREPOINT_SITE_ID = getEnvironment(
-        EnvironmentVariable.SHAREPOINT_SITE_ID,
-    );
+        const SHAREPOINT_SITE_ID = getEnvironment(
+            EnvironmentVariable.SHAREPOINT_SITE_ID,
+        );
 
-    const SHAREPOINT_DOWNLOAD_FILES_DRIVE_ID = getEnvironment(
-        EnvironmentVariable.SHAREPOINT_DOWNLOAD_FILES_DRIVE_ID,
-    );
+        const SHAREPOINT_DOWNLOAD_FILES_DRIVE_ID = getEnvironment(
+            EnvironmentVariable.SHAREPOINT_DOWNLOAD_FILES_DRIVE_ID,
+        );
 
-    const response = await client
-        .api(
-            `/sites/${SHAREPOINT_HOST_NAME},${SHAREPOINT_SITE_ID}/drives/${SHAREPOINT_DOWNLOAD_FILES_DRIVE_ID}/root/children`,
-        )
-        .expand("thumbnails")
-        .get();
+        const response = await client
+            .api(
+                `/sites/${SHAREPOINT_HOST_NAME},${SHAREPOINT_SITE_ID}/drives/${SHAREPOINT_DOWNLOAD_FILES_DRIVE_ID}/root/children`,
+            )
+            .expand("thumbnails")
+            .get();
 
-    const items = Array.isArray(response?.value) ? response.value : [];
+        const items = Array.isArray(response?.value) ? response.value : [];
 
-    const downloadFiles: DownloadFile[] = items.map((item: any): DownloadFile => {
-        
-        const thumbnails = {
-            large: item.thumbnails[0].large.url,
-            medium: item.thumbnails[0].medium.url,
-            small: item.thumbnails[0].small.url,
-        }
-        
-        return {
-            id: item.id,
-            eTag: item.eTag,
-            size: item.size,
-            fileName: item.name,
-            mimeType: item.file.mimeType,
-            downloadUrl: item["@microsoft.graph.downloadUrl"],
-            createdBy: item.createdBy?.user?.displayName ?? "Unbekannt",
-            createdAt: item.createdDateTime,
-            lastModifiedBy: item.lastModifiedBy?.user?.displayName ?? "Unbekannt",
-            lastModifiedAt: item.lastModifiedDateTime,
-            thumbnails: thumbnails,
-        };
+        const downloadFiles: DownloadFile[] = items.map((item: any): DownloadFile => {
+            
+            const thumbnails = {
+                large: item.thumbnails[0].large.url,
+                medium: item.thumbnails[0].medium.url,
+                small: item.thumbnails[0].small.url,
+            }
+            
+            return {
+                id: item.id,
+                eTag: item.eTag,
+                size: item.size,
+                fileName: item.name,
+                mimeType: item.file.mimeType,
+                downloadUrl: item["@microsoft.graph.downloadUrl"],
+                createdBy: item.createdBy?.user?.displayName ?? "Unbekannt",
+                createdAt: item.createdDateTime,
+                lastModifiedBy: item.lastModifiedBy?.user?.displayName ?? "Unbekannt",
+                lastModifiedAt: item.lastModifiedDateTime,
+                thumbnails: thumbnails,
+            };
+        });
+
+        return downloadFiles;
     });
-
-    return downloadFiles;
 }
